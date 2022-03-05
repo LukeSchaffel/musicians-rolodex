47de08df53c496a57307da9be52fb245a570d97d
LukeSchaffel <lukeschaffel@gmail.com>
2022-03-05 00:14:41 -0500

    need to figure out how to only show own contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,9 +3,32 @@ import { Contact } from "../models/contact.js"
 
 
 function index(req, res) {
-  console.log("hello me");
+ 
+  // console.log(req.user.profile);
+  Contact.find({})
+
+  //if the contacts owner matches the user, show it?
+  .then(contacts => {
+    res.render('contacts/index', {
+      contacts,
+      user: req.user.profile,
+      title: "Contacts List"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect(`/contacts`)
+  })
+}
+
+
+function newContact(req, res) {
+  res.render("contacts/new", {
+    title: "New Contact"
+  })
 }
 
 export {
-  index
+  index,
+  newContact as new
 }
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,6 +7,8 @@ const router = Router()
 
 //GET localhost:3000/contacts
 router.get('/', contactsCtrl.index)
+// GET localhost:3000/contacts/new
+router.get('/new', contactsCtrl.new)
 
 export {
   router
